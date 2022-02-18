f6b76dad69efa8d4121dd3eba6ee243533b4c26d
SmirnovaAnna <squirell0790@gmail.com>
2022-02-18 15:32:57 +0300

    add pages

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,14 @@
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
+def go_to_login_page(browser):
+    """Opens the login page
+
+    :param: browser: selenium browser object
+    
+    :return: None
+    """
     login_link = browser.find_element_by_css_selector("#login_link")
     login_link.click()
-    
+
+def test_guest_can_go_to_login_page(browser): 
+   browser.get(link) 
+   go_to_login_page(browser)     
+
